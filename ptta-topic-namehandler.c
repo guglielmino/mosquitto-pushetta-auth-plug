7780313883b1bbd8867fb5d33968115e25d6f24d
Fabrizio <guglielmino@gmail.com>
2015-03-08 10:08:27 +0100

    Updates ...

--- ptta-topic-namehandler.c
+++ ptta-topic-namehandler.c
@@ -3,6 +3,7 @@
 #include "pushetta-auth-plug.h"
 
 #define REGEX_TEXT "([^/]*)$"
+#define MAX_ERROR_MSG 100
 
 struct topic_name_hanler_data* topic_namehandler_init(){
 
@@ -10,7 +11,7 @@ struct topic_name_hanler_data* topic_namehandler_init(){
 	int status = regcomp (handler_data->r, REGEX_TEXT, REG_EXTENDED|REG_NEWLINE);
     if (status != 0) {
 		char error_message[MAX_ERROR_MSG];
-		regerror (status, r, error_message, MAX_ERROR_MSG);
+		regerror (status, handler_data->r, error_message, MAX_ERROR_MSG);
 		return NULL;
     }
     return handler_data;
@@ -23,7 +24,7 @@ char *get_channel_from_topic(struct topic_name_hanler_data* handler, const char 
     regmatch_t m[n_matches];
     char *found = NULL;
 
-	int nomatch = regexec (handler_data->r, p, n_matches, m, 0);
+	int nomatch = regexec (handler->r, p, n_matches, m, 0);
     if (nomatch) {
         printf ("No more matches.\n");
         return nomatch;
@@ -34,11 +35,13 @@ char *get_channel_from_topic(struct topic_name_hanler_data* handler, const char 
         if (m[i].rm_so == -1) {
             break;
         }
-        start = m[i].rm_so + (p - to_match);
-        finish = m[i].rm_eo + (p - to_match);
+        start = m[i].rm_so + (p - topic);
+        finish = m[i].rm_eo + (p - topic);
         
         LOG(MOSQ_LOG_NOTICE, "Match s:%d e:%d", start, finish);
     }
+
+    return found;
 }
 
 void topic_namehandler_cleanup(struct topic_name_hanler_data* handler){
