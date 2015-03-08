3275a328837fe5e9b8b7b31223e324b851d35e01
Fabrizio <guglielmino@gmail.com>
2015-03-08 15:50:06 +0100

    Updates ...

--- ptta-mysql.h
+++ ptta-mysql.h
@@ -13,3 +13,4 @@ void ptta_mysql_destroy(void *conf);
 
 struct django_auth_user *get_django_user_by_username(void *handle, const char *username);
 struct django_auth_user *get_django_user_by_token(void *handle, const char *token);
+int get_channel_owner_id(void *handle, const char *channel_name);
